d8b7b86391588fc6f06ab40405bd40c4d226dbf6
ARbind <arbind@rylyz.com>
2012-03-18 18:01:01 -0700

    trying to get hostname to resolve in heroku

--- config/initializers/pusher_services.rb
+++ config/initializers/pusher_services.rb
@@ -4,6 +4,7 @@ require 'pusher-client'
 require 'active_support/inflector'
 
 HOST = "#{Socket::gethostbyname(Socket::gethostname)[0]}" rescue "localhost"
+if ("heroku"==ENV["APP_HOST"]); HOST = "#{ENV["APP_NAME"]}.herokuapp.com" end
 
 NoOBJECT = {}
 
