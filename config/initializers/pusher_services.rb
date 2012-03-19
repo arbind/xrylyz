01adeaeee83cd550cf29c980769f06c8daf7c7e6
ARbind <arbind@rylyz.com>
2012-03-18 18:22:32 -0700

    adding hostname to heroku configs

--- config/initializers/pusher_services.rb
+++ config/initializers/pusher_services.rb
@@ -4,7 +4,7 @@ require 'pusher-client'
 require 'active_support/inflector'
 
 RYLYZ_PLAYER_HOST = Socket::gethostname rescue "localhost"
-RYLYZ_PLAYER_HOST = "#{ENV["APP_NAME"]}.herokuapp.com" unless ENV["APP_NAME"].nil?
+RYLYZ_PLAYER_HOST = "#{ENV["HEROKU_APP_NAME"]}.herokuapp.com" unless ENV["HEROKU_APP_NAME"].nil?
 puts "RYLYZ_PLAYER_HOST = #{RYLYZ_PLAYER_HOST}"
 
 NoOBJECT = {}
