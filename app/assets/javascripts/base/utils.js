dfd24532d916e48f783e15289981fab925dc9e64
ARbind <arbind@rylyz.com>
2012-05-06 19:20:10 -0700

    styling form field

---
+++ app/assets/javascripts/base/utils.js
@@ -0,0 +1,8 @@
+function elementSupportsAttribute(element, attribute) {
+  var test = document.createElement(element);
+  if (attribute in test) {
+    return true;
+  } else {
+    return false;
+  }
+};
