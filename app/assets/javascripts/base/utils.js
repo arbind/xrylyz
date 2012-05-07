d10066fefe8b42ae58af43e90519b2676b3ee987
ARbind <arbind@rylyz.com>
2012-05-07 15:37:12 -0700

    chat plays stylying started

--- app/assets/javascripts/base/utils.js
+++ app/assets/javascripts/base/utils.js
@@ -6,3 +6,27 @@ function elementSupportsAttribute(element, attribute) {
     return false;
   }
 };
+
+
+/*
+  +++ write fallback function for placeholder text
+      combine something like this with a css class and elementSupportsAttribute (to check if placeholder is supported)
+    $('.default-value').each(function() {
+
+       var default_value = this.value;
+
+       $(this).focus(function(){
+               if(this.value == default_value) {
+                       this.value = '';
+               }
+       });
+
+       $(this).blur(function(){
+               if(this.value == '') {
+                       this.value = default_value;
+               }
+       });
+
+    });
+
+*/
