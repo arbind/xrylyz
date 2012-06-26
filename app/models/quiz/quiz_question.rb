9c6e8ccbdedaf28d82f0ba48611b538de8732ca1
ARbind <arbind@rylyz.com>
2012-06-25 22:54:08 -0700

    game design

--- app/models/quiz/quiz_question.rb
+++ app/models/quiz/quiz_question.rb
@@ -2,11 +2,12 @@ class QuizQuestion
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :is_approved, :type => Boolean, :default => false
   field :question, :type => String # not nil
   field :category, :type => String # not nil
   field :info, :type => Hash
-  field :difficulty, :type => String # not nil
+  field :level, :type => Integer # not nil
+
+  field :is_approved, :type => Boolean, :default => false
 
   has_many :options
   has_many :blog_leaderboards
