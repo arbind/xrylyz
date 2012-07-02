6e4c96d4d1fb057f2d23141fdfc79705c34dd7e4
ARbind <arbind@rylyz.com>
2012-07-01 21:05:46 -0700

    fixed some syntax errors that stopped heroku from loading

---
+++ app/models/rylyz_app.rb
@@ -0,0 +1,7 @@
+class RylyzApp
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  # field :categories, :type => Array, :default => []
+  # field :app_name, :type => String, :default => "quiz"
+end
