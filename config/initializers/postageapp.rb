b2c5d00ffbade3b2bc74fa86047b9d19d29ee087
ARbind <arbind@rylyz.com>
2012-07-07 10:19:17 -0700

    rollin back postage app - maybe its breaking on heroku?

--- config/initializers/postageapp.rb
+++ config/initializers/postageapp.rb
@@ -1,4 +1,5 @@
-PostageApp.configure do |config|
-  config.api_key = 'q0cygShe1yn3h538JDYUNzNZlqbS18Zr'
-  config.recipient_override = 'you@example.com' unless RYLYZ_PLAYER_HOST.include? "rylyz.com"
-end
+# rolling back postage app - seeems to be crashing heroku?
+# PostageApp.configure do |config|
+#   config.api_key = 'q0cygShe1yn3h538JDYUNzNZlqbS18Zr'
+#   config.recipient_override = 'you@example.com' unless RYLYZ_PLAYER_HOST.include? "rylyz.com"
+# end
