83c2a0727d78dfdd4bbde9098c53eacc5887565e
Christian <neocsr@gmail.com>
2012-07-07 18:44:32 -0700

    adding leaderboard scores.

--- app/models/services/leaderboard/leaderboard_service.rb
+++ app/models/services/leaderboard/leaderboard_service.rb
@@ -1,6 +1,6 @@
 class LeaderboardService
 
-  def self.record_score(leaderboard_key, nickname, blog_url, score, member_id=nil, info={})
+  def self.record_score(leaderboard_key, nickname, blog_url, score, member_id=nil, info={}, options={})
     attributes = {
       :leaderboard_key => leaderboard_key,
       :nickname        => nickname,
@@ -10,13 +10,26 @@ class LeaderboardService
       :info            => info
     }
 
-    FinalScore.create(attributes)
+    score = FinalScore.create(attributes)
+
+    leaders = []
+    score_in_top = false
+    top = leading_players_for_game(leaderboard_key, options)
+
+    top.each do |s|
+      score_in_top = true if s.id == score.id
+      leaders << s
+    end
+    leaders << score unless score_in_top
+
+    leaders
   end
+
 
   def self.leading_players_for_game(leaderboard_key, options={})
     filter = { :leaderboard_key => leaderboard_key }
     filter[:blog_url] = options[:blog_url] if options[:blog_url]
-    limit = options[:limit] || 10
+    limit = options[:limit] || 4
 
     FinalScore.where(filter).desc(:score).limit(limit)
   end
