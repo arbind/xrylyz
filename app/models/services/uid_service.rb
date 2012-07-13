b10cde01b842c438fd5131e7185608b5e009ffbb
Arbind <arbind@rylyz.com>
2012-07-12 23:01:55 -0700

    uid salting

--- app/models/services/uid_service.rb
+++ app/models/services/uid_service.rb
@@ -1,6 +1,6 @@
 class UIDService
   require "digest/sha1"
-  def self.generate_key(value, length = 12)
-    Digest::SHA1.hexdigest(Time.now.to_s + "value")[1..length]
+  def self.generate_key(salt, length = 12)
+    Digest::SHA1.hexdigest("#{Time.now.to_s}-#{salt}")[1..length]
   end
 end
